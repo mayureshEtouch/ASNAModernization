e8d5eb0b1b37690d012bb90871220447ee901b2f
Me <sbkore_andh@yahoo.com>
2016-01-25 19:26:14 +0530

    Implemented common header/footer and added input validations js files, also implemeneted validations in Cust. sel. screen

---
+++ Themes/Current/Script/input-validations.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function () {
+
+});
+
+function validateInput(ele, buttonsToDisable, errorMessagePlaceholder) {
+    var element = $(ele);
+    if (!element.valid()) {
+        element.addClass("mdl-textfield__input_error_msg");
+        if (buttonsToDisable && buttonsToDisable.length > 0) {
+            for (var i = 0; i < buttonsToDisable.length; i++) {
+                $("#" + buttonsToDisable[i]).attr("disabled", "true");
+            }
+        }
+    } else {
+        element.removeClass("mdl-textfield__input_error_msg");
+        $("#" + errorMessagePlaceholder).hide();
+        if (buttonsToDisable && buttonsToDisable.length > 0) {
+            for (var i = 0; i < buttonsToDisable.length; i++) {
+                $("#" + buttonsToDisable[i]).removeAttr("disabled");
+            }
+        }
+    }
+}
