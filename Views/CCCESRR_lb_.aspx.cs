f0300e1b151691e33efda6c9eea5ff9f47b6a54c
santosh-kore <sbkore_andh@yahoo.com>
2016-03-09 19:17:36 +0530

    New aspx for sprint3

---
+++ Views/CCCESRR_lb_.aspx.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Conns
+{
+    public partial class CCCESRR_lb_Form : ASNA.Monarch.WebDspF.Page
+    {
+    }
+}
+
---
+++ Views/CCE2PVR_lb_.aspx.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Conns
+{
+    public partial class CCE2PVR_lb_Form : ASNA.Monarch.WebDspF.Page
+    {
+    }
+}
