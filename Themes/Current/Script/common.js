75ff59ad9a9974db6b312b31b277695ceeab08ef
santosh-kore <sbkore_andh@yahoo.com>
2016-03-15 13:57:07 +0530

    issue fixes

--- Themes/Current/Script/common.js
+++ Themes/Current/Script/common.js
@@ -56,7 +56,8 @@
                 $("#" + tableId).after("<a href='javascript:void(0);' id='next-page' style='float: right;margin-right: 15px;' class='next-icon'></a>");
             }
             if($("#CenPH__lb_SFLRCD_0").length === 1 && $("#CenPH__lb_SFLRCD_End").html() === "Bottom") {
-                var tr = "<a href='javascript:void(0);' style='float: right;margin-right: 10px;'>" + $("#CenPH__lb_SFLRCD_End").html() + "</a>"
+                $('#eof-indicator').remove();
+                var tr = "<a id='eof-indicator' style='float: right;margin-right: 10px;'>" + $("#CenPH__lb_SFLRCD_End").html() + "</a>"
                 $("#" + tableId).after(tr);
             }
         }
@@ -100,7 +101,8 @@ var generateTableWithSpanIndex = function(recordCount, tableId, direction, table
                 $("#" + tableId).after("<a href='javascript:void(0);' id='next-page' style='float: right;margin-right: 15px;' class='next-icon'></a>");
             }
             if($("#CenPH__lb_SFLRCD_0").length === 1 && $("#CenPH__lb_SFLRCD_End").html() === "Bottom") {
-                var tr = "<a href='javascript:void(0);' style='float: right;margin-right: 10px;'>" + $("#CenPH__lb_SFLRCD_End").html() + "</a>"
+                $('#eof-indicator').remove();
+                var tr = "<a id='eof-indicator' style='float: right;margin-right: 10px;'>" + $("#CenPH__lb_SFLRCD_End").html() + "</a>"
                 $("#" + tableId).after(tr);
             }
         }
