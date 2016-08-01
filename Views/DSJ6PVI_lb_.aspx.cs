70d04055533b91a88db73bfab60770ee390c3782
santosh-kore <sbkore_andh@yahoo.com>
2016-08-01 11:30:23 +0530

    new pages

---
+++ Views/DSJ6PVI_lb_.aspx.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace conns
+{
+    public partial class DSJ6PVI_lb_Form : ASNA.Monarch.WebDspF.Page
+    {
+    }
+}
