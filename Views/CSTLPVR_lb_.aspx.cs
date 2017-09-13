3eb917015156290c42ccbd0a9a79293e5cefdb99
nikita0529 <nikitathale.29@gmail.com>
2017-09-13 16:36:34 +0530

    missing updates

--- Views/CSTLPVR_lb_.aspx.cs
+++ Views/CSTLPVR_lb_.aspx.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace Conns
+namespace CONNS
 {
     public partial class CSTLPVR_lb_Form : ASNA.Monarch.WebDspF.Page
     {
