b459b96fb428eaa83ff98e9d24193db046922ebd
nikita0529 <nikitathale.29@gmail.com>
2017-11-03 18:30:59 +0530

    progressive order btn added

--- Views/DSU3DFR_lb_.aspx.cs
+++ Views/DSU3DFR_lb_.aspx.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace conns
+namespace CONNS
 {
     public partial class DSU3DFR_lb_Form : ASNA.Monarch.WebDspF.Page
     {
